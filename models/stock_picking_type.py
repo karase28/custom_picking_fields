7c052589c56e122537bd822684be36e005442e88
karase28 <jkopacz@os.pl>
2025-08-09 22:11:20 +0200

    poprawiłem wg polityki odoo18

--- models/stock_picking_type.py
+++ models/stock_picking_type.py
@@ -3,15 +3,33 @@ from odoo import models, fields
 class PickingType(models.Model):
     _inherit = 'stock.picking.type'
 
-    code = fields.Selection(selection_add=[
-        ('manufacturing', 'Manufacturing'),
-        ('returns', 'Returns')
-    ], ondelete={'code':'cascade'})
+    code = fields.Selection(
+        selection_add=[
+            ('manufacturing', 'Manufacturing'),
+            ('returns', 'Returns')
+        ],
+        ondelete={
+            'manufacturing': 'cascade',
+            'returns': 'cascade'
+        }
+    )
 
-    return_type = fields.Selection([
-        ('none', 'None'),
-        ('return', 'Return'),
-        ('refund', 'Refund'),
-    ], string='Return Type', default='none', ondelete={'return_type':'cascade'})
+    return_type = fields.Selection(
+        [
+            ('none', 'None'),
+            ('return', 'Return'),
+            ('refund', 'Refund'),
+        ],
+        string='Return Type',
+        default='none',
+        ondelete={
+            'none': 'set default',
+            'return': 'cascade',
+            'refund': 'cascade'
+        }
+    )
 
-    show_reserved_qty = fields.Boolean(string='Show Reserved Quantity', default=False, ondelete={'show_reserved_qty':'cascade'})
+    show_reserved_qty = fields.Boolean(
+        string='Show Reserved Quantity',
+        default=False
+    )
